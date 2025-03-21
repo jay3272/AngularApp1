191d246ce060bb592e67f65135829b013e484f6e
吳介 <jaywu3272@gmail.com>
2025-03-21 13:37:52 +0800

    web api使用

--- AngularApp1.Server/obj/Debug/net8.0/AngularApp1.Server.AssemblyInfo.cs
+++ AngularApp1.Server/obj/Debug/net8.0/AngularApp1.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AngularApp1.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34284ec2e9307ddbf8a4a249c67002e74b3497e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AngularApp1.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AngularApp1.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
