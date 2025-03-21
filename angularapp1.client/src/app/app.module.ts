94f214472914de91c077f002777e57c694996ae4
吳介 <jaywu3272@gmail.com>
2025-03-21 14:11:37 +0800

    建立相關模組
    ng g c login
    ng g c login
    ng g c layout
    ng g c employee
    ng g c dashboard
    ng g c project
    ng g c projectEmployee

--- angularapp1.client/src/app/app.module.ts
+++ angularapp1.client/src/app/app.module.ts
@@ -4,10 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { EmployeeComponent } from './pages/employee/employee.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProjectComponent } from './pages/project/project.component';
+import { ProjectEmployeesComponent } from './pages/project-employees/project-employees.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LayoutComponent,
+    EmployeeComponent,
+    DashboardComponent,
+    ProjectComponent,
+    ProjectEmployeesComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
