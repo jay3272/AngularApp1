5eb94cacc47e6df8ff46bf95d48dc2f6a10f5a68
吳介 <jaywu3272@gmail.com>
2025-03-21 15:31:23 +0800

    add route, login page
    將首頁導至Login，

--- angularapp1.client/src/app/app-routing.module.ts
+++ angularapp1.client/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component'
+import { DashboardComponent } from './pages/dashboard/dashboard.component'
+import { EmployeeComponent } from './pages/employee/employee.component'
+import { LayoutComponent } from './pages/layout/layout.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent},
+  { path: 'employee', component: EmployeeComponent },
+  { path: 'layout', component: LayoutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
